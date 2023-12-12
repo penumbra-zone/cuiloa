d0450a9768ed5fdc9a58c76cd1512bd2b684997f
ejmg <elias.jm.garcia@gmail.com>
2023-12-12 14:09:54 -0600

    skeleton code for blocks table

--- src/app/blocks/page.tsx
+++ src/app/blocks/page.tsx
@@ -1,7 +1,34 @@
-const Page = async () => {
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const Page = () => {
+  const { data, isFetched, isError } = useQuery({
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/blocks?page=${0}`);
+      return data;
+    },
+    queryKey: ["blocksQuery"],
+    retry: false,
+  });
+
+  if (isError) {
+    return (
+      <div>
+        <h1 className="text-lg font-bold">Error occurred while querying Blocks data...</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-lg font-bold">Not yet implemented...</h1>
+      { isFetched && (Boolean(data)) ? (
+        <pre>{JSON.stringify(data)}</pre>
+      ) : (
+        <p>no data...</p>
+      )}
     </div>
   );
 };
