fcd3ca5b9068b6bf259018f7a71cb60776986e6d
ejmg <elias.jm.garcia@gmail.com>
2024-01-16 20:10:08 -0600

    stylings

--- src/app/ibc/channel/[id]/page.tsx
+++ src/app/ibc/channel/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { type FC } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface PageProps {
   params: {
@@ -17,7 +18,7 @@ const Page : FC<PageProps> = ({ params }) => {
       console.log(`Fetching: GET /api/ibc/channel/${channelId}`);
       const { data } = await axios.get(`/api/ibc/channel?q=${channelId}`);
       console.log("Fetched result:", data);
-      return data;
+      return data as { connectionId: string, clientId: string, recentTransactions: Array<{hash: string}>};
       // TODO: enforce validation
       // const result = IbcChannelValidator.safeParse(data);
     },
@@ -39,12 +40,29 @@ const Page : FC<PageProps> = ({ params }) => {
   return (
     <div>
       {isFetched ? (
-        <div>
+        <div className="flex flex-col justify-center w-full">
           <h1 className="text-3xl mx-auto py-5 font-semibold">IBC Channel</h1>
         {// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
         data  ? (
-          <div className="flex flex-col justify-center w-full">
-            <pre>{JSON.stringify(data, undefined, 2)}</pre>
+          <div className="bg-white rounded-sm flex flex-wrap justify-between p-5 max-w-5xl ml-auto mr-auto gap-2">
+            <div className="flex justify-start w-full">
+              <p className="w-1/6">Channel ID</p>
+              <pre>{channelId}</pre>
+            </div>
+            <div className="flex justify-start w-full">
+              <p className="w-1/6">Client ID</p>
+              <Link href={`/ibc/client/${data.clientId}`} className="underline"><pre>{data.clientId}</pre></Link>
+            </div>
+            <div className="flex justify-start w-full">
+              <p className="w-1/6">Connection IDs</p>
+              <Link href={`/ibc/connection/${data.connectionId}`} className="underline"><pre>{data.connectionId}</pre></Link>
+            </div>
+            <div className="flex justify-start w-full">
+              <p className="w-1/6">Recent Transactions</p>
+              <div className="overflow-hidden">
+                {data.recentTransactions.map(({ hash }, i) => <Link href={`/transaction/${hash}`} key={i} className="underline"><pre>{hash}</pre></Link>)}
+              </div>
+            </div>
           </div>
         ) : (
           <p>No results</p>
