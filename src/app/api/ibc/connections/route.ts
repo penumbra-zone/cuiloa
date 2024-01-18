e33760a020eb1023305177864f5e326b2dc21f65
ejmg <elias.jm.garcia@gmail.com>
2024-01-17 21:22:17 -0600

    basic skeleton for enumerating existing ibc connections + api endpoint

---
+++ src/app/api/ibc/connections/route.ts
@@ -0,0 +1,44 @@
+import db from "@/lib/db";
+import { type NextRequest } from "next/server";
+
+export async function GET(_req: NextRequest) {
+  console.log("SUCCESS: GET /api/ibc/connections");
+  try {
+    console.log("Querying indexer for IBC connections");
+
+    const connectionsQuery = await db.events.findMany({
+      select: {
+        attributes: {
+          select: {
+            key: true,
+            value: true,
+          },
+          where: {
+            key: {
+              equals: "connection_id",
+            },
+          },
+        },
+      },
+      where: {
+        type: {
+          equals: "connection_open_init",
+        },
+      },
+      orderBy: {
+        rowid: "desc",
+      },
+    });
+
+    console.log("Successfully queried for IBC connections.", connectionsQuery);
+    console.log(connectionsQuery[0].attributes);
+
+    const connections = connectionsQuery.map(({ attributes }) => attributes).flat();
+
+    return new Response(JSON.stringify(connections));
+
+  } catch (error) {
+    console.error("GET request failed.", error);
+    return new Response("Could not query IBC Connections.", { status: 500 });
+  }
+}
