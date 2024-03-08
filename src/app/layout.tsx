0de7730bfece4e1680e7c72a0ba3bfd82c59c168
ejmg <elias.jm.garcia@gmail.com>
2024-03-08 17:47:38 -0600

    added favicon, added link to source code, fixed padding on navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/lib/utils";
 import "@/lib/patch-toJSON-BigInt";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { CodeIcon } from "lucide-react";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Cuiloa",
@@ -14,16 +16,28 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("bg-white text-slate-900 dark:bg-slate-900 dark:text-white antialiased", ioveskaSans.variable, ioveskaTerm.variable)}>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 dark:bg-slate-900 dark:text-white antialiased",
+        ioveskaSans.variable,
+        ioveskaTerm.variable,
+      )}
+    >
       <body className="min-w-full min-h-screen">
         <Providers>
           <Navbar />
-          <div className="container xs-container min-h-[668px]">
-            <div className="bg-slate-100 p-1 sm:p-2 rounded-lg border-solid border-[1px] border-slate-200 shadow-sm">
-            {children}
+          <div className="flex flex-col container xs-container justify-start min-h-[640px]">
+            <div className="bg-slate-100 p-1 sm:p-2 rounded-lg border-solid border-[1px] border-slate-200 shadow-sm mb-auto">
+              {children}
+            </div>
+            <div className="self-center py-5">
+              <Link href="https://github.com/penumbra-zone/cuiloa">
+                <CodeIcon className="w-4" />
+              </Link>
             </div>
           </div>
         </Providers>
