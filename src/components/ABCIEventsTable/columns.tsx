b4020e678d88ccf2015b5072e05e1c820860a2b1
ejmg <elias.jm.garcia@gmail.com>
2024-03-12 22:50:57 -0500

    updated stylings for /blocks/[HEIGHT] view and ABCIEventsTable to match new responsive ui styles. reduced xs-container var to have smaller y padding.

--- src/components/ABCIEventsTable/columns.tsx
+++ src/components/ABCIEventsTable/columns.tsx
@@ -11,26 +11,26 @@ export type ABCIEventsColumns = Record<number, {
 export const columns : Array<ColumnDef<ABCIEventsColumns>> = [
   {
     accessorKey: "type",
-    header: () => <div className="font-semibold text-gray-800 text-center sm:text-lg text-sm">Type</div>,
+    header: () => <div className="font-semibold text-gray-800 text-center sm:text-base text-sm">Type</div>,
     cell: ({ getValue }) => {
       const abciType = getValue() as string;
-      return <pre className="text-center">{abciType}</pre>;
+      return <pre className="text-center sm:whitespace-normal whitespace-pre-wrap sm:break-normal break-all sm:w-auto sm:text-sm text-xs">{abciType}</pre>;
     },
   },
   {
     accessorKey: "key",
-    header: () => <div className="font-semibold text-gray-800 text-center sm:text-lg text-sm">Key</div>,
+    header: () => <div className="font-semibold text-gray-800 text-center sm:text-base text-sm">Key</div>,
     cell: ({ getValue }) => {
       const abciKey = getValue() as string;
-      return <pre className="text-center">{abciKey}</pre>;
+      return <pre className="sm:whitespace-normal whitespace-pre-wrap sm:break-normal break-all text-center sm:text-sm text-xs">{abciKey}</pre>;
     },
   },
   {
     accessorKey: "value",
-    header: () => <div className="font-semibold text-gray-800 text-center sm:text-lg text-sm">Value</div>,
+    header: () => <div className="font-semibold text-gray-800 text-center sm:text-base text-sm">Value</div>,
     cell: ({ getValue }) => {
       const abciValue : string = getValue() as string | null ?? "None";
-      return <pre className="text-center">{abciValue}</pre>;
+      return <pre className="text-center break-all whitespace-pre-wrap sm:w-auto w-[120px] sm:text-sm text-xs">{abciValue}</pre>;
     },
   },
 ];
