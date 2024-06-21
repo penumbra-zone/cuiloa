4d543364d3f792e5657531e0b960247931db2a8f
ejmg <elias.jm.garcia@gmail.com>
2024-06-21 17:00:59 -0500

    moved font assets into src to ensure that fonts are compiled into the nextjs application + updated exported font functions.

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -4,92 +4,92 @@ export const ioveskaSans = localFont({
   variable: "--font-iosevka-sans",
   src: [
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Bold.woff2",
+      path: "../fonts/IosevkaSans-Bold.woff2",
       style: "normal",
       weight: "700",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-BoldItalic.woff2",
+      path: "../fonts/IosevkaSans-BoldItalic.woff2",
       style: "italic",
       weight: "700",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-ExtraBold.woff2",
+      path: "../fonts/IosevkaSans-ExtraBold.woff2",
       style: "normal",
       weight: "800",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-ExtraBoldItalic.woff2",
+      path: "../fonts/IosevkaSans-ExtraBoldItalic.woff2",
       style: "italic",
       weight: "800",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-ExtraLight.woff2",
+      path: "../fonts/IosevkaSans-ExtraLight.woff2",
       style: "normal",
       weight: "200",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-ExtraLightItalic.woff2",
+      path: "../fonts/IosevkaSans-ExtraLightItalic.woff2",
       style: "italic",
       weight: "200",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Heavy.woff2",
+      path: "../fonts/IosevkaSans-Heavy.woff2",
       style: "normal",
       weight: "900",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-HeavyItalic.woff2",
+      path: "../fonts/IosevkaSans-HeavyItalic.woff2",
       style: "italic",
       weight: "900",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Italic.woff2",
+      path: "../fonts/IosevkaSans-Italic.woff2",
       style: "italic",
       weight: "400",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Light.woff2",
+      path: "../fonts/IosevkaSans-Light.woff2",
       style: "normal",
       weight: "300",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-LightItalic.woff2",
+      path: "../fonts/IosevkaSans-LightItalic.woff2",
       style: "italic",
       weight: "300",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Medium.woff2",
+      path: "../fonts/IosevkaSans-Medium.woff2",
       style: "normal",
       weight: "500",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-MediumItalic.woff2",
+      path: "../fonts/IosevkaSans-MediumItalic.woff2",
       style: "italic",
       weight: "500",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Regular.woff2",
+      path: "../fonts/IosevkaSans-Regular.woff2",
       style: "normal",
       weight: "400",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-SemiBold.woff2",
+      path: "../fonts/IosevkaSans-SemiBold.woff2",
       style: "normal",
       weight: "600",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-SemiBoldItalic.woff2",
+      path: "../fonts/IosevkaSans-SemiBoldItalic.woff2",
       style: "italic",
       weight: "600",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-Thin.woff2",
+      path: "../fonts/IosevkaSans-Thin.woff2",
       style: "normal",
       weight: "100",
     },
     {
-      path: "../../public/fonts/WOFF2/IosevkaSans-ThinItalic.woff2",
+      path: "../fonts/IosevkaSans-ThinItalic.woff2",
       style: "italic",
       weight: "100",
     },
@@ -98,7 +98,7 @@ export const ioveskaSans = localFont({
 
 export const ioveskaTerm = localFont({
   variable: "--font-iosevka-mono",
-  src: "../../public/fonts/WOFF2/IosevkaTerm-Regular.woff2",
+  src: "../fonts/IosevkaTerm-Regular.woff2",
   style: "normal",
   weight: "400",
 });
