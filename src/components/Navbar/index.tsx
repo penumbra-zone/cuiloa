128d52749704d388bf3d8d798e77f94877212ecb
ejmg <elias.jm.garcia@gmail.com>
2024-07-26 19:25:34 -0500

    Made searchbar smaller to match mocks, theme toggle button fully round, removed default export, added radiant logo and website subtitle.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,20 +3,34 @@
 import { type FC } from "react";
 import SearchBar from "../Searchbar";
 import Link from "next/link";
-
+import Image from "next/image";
 import { ThemeToggleButton } from "../ThemeToggleButton";
+import radiantLogoRainbow from "./radiant-logo-rainbow.png";
 
-
-const Navbar : FC = () => {
+const RadiantLogoRainbow = ({ width, height, className } : { width: number, height: number, className?: string }) => {
   return (
-  <div className="flex items-center justify-between py-5 max-w-[1200px] ml-auto mr-auto">
-    <h1 className="font-bold text-lg py-5"><Link href="/">Cuiloa Block Explorer</Link></h1>
-    <div className="w-5/6 sm:max-w-lg">
-      <SearchBar/>
+    <div className={className}>
+      <Image src={radiantLogoRainbow} alt="RadiantCommons.com Logo" width={width} height={height}/>
     </div>
-    <ThemeToggleButton />
-  </div>
   );
 };
 
-export default Navbar;
+export const Navbar : FC = () => {
+  return (
+  <div className="flex justify-between items-center py-8 max-w-[1200px] mx-auto">
+    <div className="flex-grow flex flex-wrap items-baseline">
+      <Link href="https://radiantcommons.com" className="self-center" >
+        <RadiantLogoRainbow height={24} width={24}/>
+      </Link>
+      <h1 className="font-semibold text-2xl ml-1 mr-3"><Link href="/">Cuiloa</Link></h1>
+      <Link className="text-link font-medium text-bas" href="https://penumbra.zone/">
+        A Block Explorer For Penumbra
+      </Link>
+    </div>
+    <div className="flex items-center gap-2">
+      <SearchBar className="w-5/6 sm:max-w-40"/>
+      <ThemeToggleButton />
+    </div>
+  </div>
+  );
+};
