0aa2655c336f2a0da854ea2e47e7406915363684
ejmg <elias.jm.garcia@gmail.com>
2024-07-27 22:08:13 -0500

    Removed all IosevkaSans font files and config related stuff, added WorkSans for display, Poppins for body, and reconfigured for them.

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -1,99 +1,19 @@
+import { Poppins, Work_Sans } from "next/font/google";
 import localFont from "next/font/local";
 
-export const ioveskaSans = localFont({
-  variable: "--font-iosevka-sans",
-  src: [
-    {
-      path: "../fonts/IosevkaSans-Bold.woff2",
-      style: "normal",
-      weight: "700",
-    },
-    {
-      path: "../fonts/IosevkaSans-BoldItalic.woff2",
-      style: "italic",
-      weight: "700",
-    },
-    {
-      path: "../fonts/IosevkaSans-ExtraBold.woff2",
-      style: "normal",
-      weight: "800",
-    },
-    {
-      path: "../fonts/IosevkaSans-ExtraBoldItalic.woff2",
-      style: "italic",
-      weight: "800",
-    },
-    {
-      path: "../fonts/IosevkaSans-ExtraLight.woff2",
-      style: "normal",
-      weight: "200",
-    },
-    {
-      path: "../fonts/IosevkaSans-ExtraLightItalic.woff2",
-      style: "italic",
-      weight: "200",
-    },
-    {
-      path: "../fonts/IosevkaSans-Heavy.woff2",
-      style: "normal",
-      weight: "900",
-    },
-    {
-      path: "../fonts/IosevkaSans-HeavyItalic.woff2",
-      style: "italic",
-      weight: "900",
-    },
-    {
-      path: "../fonts/IosevkaSans-Italic.woff2",
-      style: "italic",
-      weight: "400",
-    },
-    {
-      path: "../fonts/IosevkaSans-Light.woff2",
-      style: "normal",
-      weight: "300",
-    },
-    {
-      path: "../fonts/IosevkaSans-LightItalic.woff2",
-      style: "italic",
-      weight: "300",
-    },
-    {
-      path: "../fonts/IosevkaSans-Medium.woff2",
-      style: "normal",
-      weight: "500",
-    },
-    {
-      path: "../fonts/IosevkaSans-MediumItalic.woff2",
-      style: "italic",
-      weight: "500",
-    },
-    {
-      path: "../fonts/IosevkaSans-Regular.woff2",
-      style: "normal",
-      weight: "400",
-    },
-    {
-      path: "../fonts/IosevkaSans-SemiBold.woff2",
-      style: "normal",
-      weight: "600",
-    },
-    {
-      path: "../fonts/IosevkaSans-SemiBoldItalic.woff2",
-      style: "italic",
-      weight: "600",
-    },
-    {
-      path: "../fonts/IosevkaSans-Thin.woff2",
-      style: "normal",
-      weight: "100",
-    },
-    {
-      path: "../fonts/IosevkaSans-ThinItalic.woff2",
-      style: "italic",
-      weight: "100",
-    },
-  ],
+// NOTE: The approach taken for handling multiple fonts is that we globally insert poppins (all normal text) and IoveskaTerm (pre/code/technical info)
+//       but only import workSans for the Navbar because it is used exclusively as a display type font.
+
+export const poppins = Poppins({
+  // We might not actually need all of these weights. I just selected defaulted to normal, medium, semi, and bold.
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
 });
 
 export const ioveskaTerm = localFont({
