1e34ce224626242d9f0a3f31685c23f4bf6a9a3a
ejmg <elias.jm.garcia@gmail.com>
2024-07-29 17:39:05 -0500

    outline of mock for the new landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,55 +2,88 @@ import Link from "next/link";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 import { Button } from "@/components/ui/button";
+import { FC } from "react";
+
+interface CardProps {
+  buttonText: string,
+  buttonLink: string,
+  children?: React.ReactNode,
+  className?: string,
+  heading: string,
+}
+
+const LandingCard: FC<CardProps> = ({ heading, children, className, buttonText, buttonLink }) => {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle className="text-lg font-medium">{heading}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-1 items-center w-full">
+        {children}
+        <Button className="w-full" asChild>
+          <Link href={buttonLink}>{buttonText}</Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default async function Home() {
-  // Idea: break out the buttons to their own cards where each uses the description to help describe the data to users.
   return (
-    <div className="flex gap-5 items-center justify-center py-5">
-      <div>
-        <Card>
-          <CardHeader className="">
-            <CardTitle className="text-center text-lg sm:text-2xl">
-              Penumbra is constantly evolving...
-            </CardTitle>
-            <CardDescription className="text-center">
-              Navigate and explore public chain data with the links below.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-1 items-center">
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/transactions">Recent Transactions</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/blocks">Recent Blocks</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/ibc/clients">IBC Clients</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/ibc/channels">IBC Channels</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/ibc/connections">IBC Connections</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/staking">Staking</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/dex">Dex</Link>
-            </Button>
-            <Button className="sm:w-2/3 w-full" asChild>
-              <Link href="/governance">Governance</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
+    <div className="flex flex-wrap gap-3 items-center justify-between py-5">
+      <LandingCard
+        heading="Transactions"
+        buttonLink="/transactions"
+        buttonText="Explore Transactions"
+        className="basis-[45%] w-auto"
+      />
+      <LandingCard
+        heading="Blocks"
+        buttonLink="/blocks"
+        buttonText="Explore Blocks"
+        className="basis-[45%] w-auto"
+      />
+      <LandingCard
+        heading="IBC Clients"
+        buttonLink="/ibc/clients"
+        buttonText="Explore Clients"
+        className="w-[380px]"
+      />
+      <LandingCard
+        heading="IBC Connections"
+        buttonLink="/ibc/connections"
+        buttonText="Explore Connections"
+        className="w-[380px]"
+      />
+      <LandingCard
+        heading="IBC Channels"
+        buttonLink="/ibc/channels"
+        buttonText="Explore Channels"
+        className="w-[380px]"
+      />
+      <LandingCard
+        heading="Staking"
+        buttonLink="/staking"
+        buttonText="Explore Staking"
+        className="w-[380px]"
+      />
+      <LandingCard
+        heading="Dex"
+        buttonLink="/dex"
+        buttonText="Explore Dex"
+        className="w-[380px]"
+      />
+      <LandingCard
+        heading="Governance"
+        buttonLink="/governance"
+        buttonText="Explore Governance"
+        className="w-[380px]"
+      />
     </div>
   );
 }
